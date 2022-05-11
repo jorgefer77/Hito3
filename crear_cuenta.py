f356b7b9308aabdc76c89075368bb27236538cce
jorgefer77 <82996206+jorgefer77@users.noreply.github.com>
2022-05-11 14:41:08 -0400

    Update crear_cuenta.py

--- crear_cuenta.py
+++ crear_cuenta.py
@@ -1,17 +1,22 @@
-from selenium import webdriver
+#se ocupo selenium
+from selenium import webdriver 
+#datos a ingresar      
+correo = "yetone7093@eoscast.com"  #nombre de usuario que corresponde al correo
+telefono= "+56966666666"
+password = "LA37gl85xp2f" #contraseña
+#pagina
+link = "https://www.serviceshop.cl/customer/registration"
 
-email="margie11i_j620w@hxsni.com"
-tel="95456431"
-password="123456"
-url="https://www.serviceshop.cl/customer/registration" 
+#Automatizacion de inicio de sesion (nombre de usuario y contraseña)
+driver = webdriver.Chrome("/home/coke/Documentos/Hito2/chromedriver")
 
-driver= webdriver.Chrome("/Users/Desktop/chromedriver")
-driver.get(url)
+driver.get(link)
 
-driver.find_element_by_name("mail").send_keys(mail)
-driver.find_element_by_name("tel").send_keys(tel)
-driver.find_element_by_name("password").send_keys(password)
+driver.find_element_by_name("customer[email]").send_keys(correo)
+
+driver.find_element_by_name("customer[phone]").send_keys(telefono)
+driver.find_element_by_name("customer[password]").send_keys(password)
+driver.find_element_by_name("customer[password_confirmation]").send_keys(password)
 driver.find_element_by_css_selector("input[type=\"submit\" i]").click()
 
-print("efectuo correctamente")
-
+driver.close()
