6efb669d37a2f696e1925290cd39831c9fc8c128
jorgefer77 <82996206+jorgefer77@users.noreply.github.com>
2022-05-10 21:47:08 -0400

    Update inicio_sesion.py

--- inicio_sesion.py
+++ inicio_sesion.py
@@ -1,14 +1,21 @@
-from selenium import webdriver
+#se ocupo selenium
+from selenium import webdriver 
+#datos a ingresar      
+correo = "yetone7093@eoscast.com "  #nombre de usuario que corresponde al correo
+password = "LA37gl85xp2f" #contraseña, se va ir probando y realizando el ataque 
+#pagina
+link = "https://www.serviceshop.cl/customer/login"
 
-email="margie11i_j620w@hxsni.com"
-password="123456"
-url="https://www.serviceshop.cl/customer/login" 
+#Automatizacion de inicio de sesion (nombre de usuario y contraseña)
+driver = webdriver.Chrome("/home/coke/Documentos/Hito2/chromedriver")
 
-driver= webdriver.Chrome("/Users/Desktop/chromedriver")
-driver.get(url)
+driver.get(link)
 
-driver.find_element_by_name("mail").send_keys(mail)
-driver.find_element_by_name("password").send_keys(password)
+driver.find_element_by_name("customer[email]").send_keys(correo)
+
+driver.find_element_by_name("customer[password]").send_keys(password)
+
 driver.find_element_by_css_selector("input[type=\"submit\" i]").click()
 
-print("efectuo correctamente")
+driver.close()
+
