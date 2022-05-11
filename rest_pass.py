f5a088bc2b1916811274a8e3f4798b1638276ac3
jorgefer77 <82996206+jorgefer77@users.noreply.github.com>
2022-05-11 14:42:40 -0400

    Update rest_pass.py

--- rest_pass.py
+++ rest_pass.py
@@ -1,13 +1,19 @@
-from selenium import webdriver
+#se ocupo selenium
+from selenium import webdriver 
+#datos a ingresar      
+correo = "yetone7093@eoscast.com"  #nombre de usuario que corresponde al correo
+#pagina
+link = "https://www.serviceshop.cl/customer/login"
 
-email="margie11i_j620w@hxsni.com"
+#Automatizacion de inicio de sesion (nombre de usuario y contraseña)
+driver = webdriver.Chrome("/home/coke/Documentos/Hito2/chromedriver")
 
-url="https://www.serviceshop.cl/customer/login"
+driver.get(link)
 
-driver= webdriver.Chrome("/Users/Desktop/chromedriver")
-driver.get(url)
+driver.find_element_by_id("reset_password").click()
+
+driver.find_element_by_name("customer[email]").send_keys(correo)
+
 driver.find_element_by_css_selector("input[type=\"submit\" i]").click()
-driver.find_element_by_name("mail").send_keys(mail)
 
-
-print("efectuo correctamente")
+driver.close()
